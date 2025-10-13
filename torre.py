c514a010480d9ecf687168b3d13c541d74f0a805
Tiago Mattos <tiagolmattos06@gmail.com>
2025-10-13 18:28:14 -0300

    Update torre.py

--- torre.py
+++ torre.py
@@ -1,16 +1,15 @@
 #definir função
 
 def torre(n,t_inicial, t_auxiliar, t_final):
-    if n == 1:
-        print(f'mova disco {n} de {t_inicial} para {t_final}')
+    if n == 0:
         return
     torre(n-1,t_inicial, t_final, t_auxiliar)
-    print(f'mova disco {n} de {t_inicial} para {t_final}')
+    print(f'Mova disco {n} da {t_inicial} para a {t_final}')
     torre(n-1, t_auxiliar, t_inicial, t_final)
 
 
 n = int(input("Digite o número de discos: "))
-t_inicial = "Torre 1"
-t_auxiliar = "Torre 2"
-t_final = "Torre 3"
+t_inicial = "Torre Inicial"
+t_auxiliar = "Torre Auxiliar"
+t_final = "Torre Destino"
 torre(n,t_inicial, t_auxiliar, t_final)
